4a4b0d0b94b9a2cce2ece465d99c13bf7c0fcd92
David Fletcher <david_fletcher@taylor.edu>
2019-04-04 00:27:23 -0400

    Initial work on project

---
+++ src/components/Card.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-card tile>
+    <v-img src="https://www.futurity.org/wp/wp-content/uploads/2018/04/pink-flowers-against-green_1600.jpg" />
+    <v-card-title primary-title>
+      <h3 class="display-2">{{ title }}</h3>
+    </v-card-title>
+    <v-card-text>
+      {{ text }}
+    </v-card-text>
+    <v-card-actions>
+      <v-btn color="primary" flat>Read More</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    text: {
+      type: String,
+      required: false
+    }
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-container fluid grid-list-md>
+    <v-layout row wrap>
+      <v-jumbotron color="primary" dark>
+        <v-container fill-height>
+          <v-layout align-center>
+            <v-flex text-xs-center>
+              <h3 class="display-4">Vindicating Vixens</h3>
+              <p class="display-1">A second-look at some controversial women in the Bible.</p>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-jumbotron>
+      <!-- <v-flex xs12 md6 lg3>
+        <Card v-bind:title="'Martha'" v-bind:text="'Martha was a sweet woman who did not want to harm anyone.'"/>
+        <Card v-bind:title="'Martha'" v-bind:text="'Martha was a sweet woman who did not want to harm anyone.'"/>
+        <Card v-bind:title="'Martha'" v-bind:text="'Martha was a sweet woman who did not want to harm anyone.'"/>
+      </v-flex> -->
+      <v-flex
+              xs12
+              md6
+              lg3
+              v-for="n in 12"
+              :key="n"
+            >
+              <Card v-bind:title="'Martha'" v-bind:text="'Martha was a sweet woman who did not want to harm anyone.'"/>
+            </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import Card from './Card';
+
+export default {
+  name: 'Home',
+  components: {
+    Card
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
